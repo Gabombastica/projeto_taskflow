f8776d8b1bef4d543ccfb667574d32ed81d3b935
senai_26_27 <gabypratesx@gmail.com>
2026-08-20 15:31:14 -0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,54 @@
+# Importe a Biblioteca
+from flask import Flask, render_template, request
+
+# Criar objeto flask "apelido - app"
+app = Flask(__name__)
+
+# Base fake
+base_fake = []
+
+# Rotas
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/pessoa')
+def pessoa():
+    return render_template('pessoa.html')
+
+
+@app.route('/atividade/criar', methods=['GET', 'POST'])
+def criar_atividade():
+    if request.method == 'POST':
+        # Aqui recebe dados do Formulário
+        nome_atividade = request.form.get('form_nome')
+        descricao_atividade = request.form.get('form_descricao')
+        data_atividade = request.form.get('form_data')
+        prioridade_atividade = request.form.get('form_prioridade')
+        categoria_atividade = request.form.getlist('form_categoria')
+
+        dados = {
+            'nome_atividade': nome_atividade,
+            'descricao_atividade': descricao_atividade,
+            'data_atividade': data_atividade,
+            'prioridade_atividade': prioridade_atividade,
+            'categoria_atividade': categoria_atividade
+        }
+        print(f"dados cadastrados: {dados}")
+        base_fake.append(dados)
+        print(f"base fake: {base_fake}")
+        return render_template('listar_atividades.html', dados_atividade = base_fake)
+
+    return render_template('criar_atividade.html')
+
+
+@app.route('/atividades/listar')
+def listar_atividades():
+    return render_template('listar_atividades.html', dados_atividade = base_fake)
+
+
+# Iniciar aplicação web
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=5001)
+# Nada deve ser colocado abaixo
