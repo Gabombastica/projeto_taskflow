52f1d34f2a318a2f1d14c801113c9afca2c9869c
senai_26_27 <gabypratesx@gmail.com>
2026-09-24 09:39:54 -0300

    initial

--- models.py
+++ models.py
@@ -5,10 +5,20 @@ from sqlalchemy.orm import relationship, sessionmaker, declarative_base
 # Base de dados - endereço
 engine = create_engine('mysql+pysql://root:senaisp@localhost:3306/taskflow')
 
-# Config sessão
-local_session = sessionmaker(bind=engine)
+# Configurar sessão
+db_session = sessionmaker(bind=engine)
 
 Base = declarative_base()
 
 class Pessoa(Base):
     __tablename__ = 'pessoas'
+    id = Column(Integer, primary_key=True) # Poderia ser id_pessoa
+    nome = Column(String(50), nullable=False)
+    email = Column(String(50), nullable=False, unique=True)
+    senha_hash = Column(String(20), nullable=False)
+    papel = Column(String(100), default='usuario', nullable=False)
+    criado_em = Column(DateTime, nullable=False, server_default=func.now())
+
+    def __repr__(self):
+        return f'Pessoa {self.nome}'
+
